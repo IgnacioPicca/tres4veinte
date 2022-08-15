0e10be7c9f9eaca10ae9993bc3b8f983fdb58978
Ignacio Picca <piccaignacio@gmail.com>
2022-08-15 14:28:06 -0300

    animacion entrada de elementos

--- tres4veinte/js/app.js
+++ tres4veinte/js/app.js
@@ -1,13 +1,18 @@
-const form = document.getElementById('form');
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const email = document.getElementById('email');
-    if (email.value.match(/[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i.test())) {
-        alert('El email es correcto');
-    } else {
-        alert('El email es incorrecto');
+// Aparicion de elementos
+
+window.addEventListener('scroll', function () {                         //Al hacer scroll, se ejecuta la funcion
+    let elements = document.getElementsByClassName('scroll-content');   //Se obtienen todos los elementos con la clase scroll-content
+    let screenSize = window.innerHeight;                                //Se obtiene el tamaño de la pantalla               
+
+    for (var i = 0; i < elements.length; i++) {                         //Se recorren todos los elementos
+        let element = elements[i];                                      //Se obtiene el elemento actual
+
+        if (element.getBoundingClientRect().top < screenSize) {         //Si el elemento esta en la pantalla
+            element.classList.add('visible');                           //Se agrega la clase visible
+        } else {
+            element.classList.remove('visible');                        //Si no, se quita la clase visible
+        }
     }
-}
-);
+});
 
