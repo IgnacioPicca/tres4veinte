20ac5c29522e54a311d2dd19599db0613a7dab32
Ignacio Picca <piccaignacio@gmail.com>
2022-08-15 21:12:36 -0300

    var js

--- tres4veinte/js/app.js
+++ tres4veinte/js/app.js
@@ -2,11 +2,11 @@
 // Aparicion de elementos
 
 window.addEventListener('scroll', function () {                         //Al hacer scroll, se ejecuta la funcion
-    let elements = document.getElementsByClassName('scroll-content');   //Se obtienen todos los elementos con la clase scroll-content
-    let screenSize = window.innerHeight;                                //Se obtiene el tamaño de la pantalla               
+    var elements = document.getElementsByClassName('scroll-content');   //Se obtienen todos los elementos con la clase scroll-content
+    var screenSize = window.innerHeight;                                //Se obtiene el tamaño de la pantalla               
 
     for (var i = 0; i < elements.length; i++) {                         //Se recorren todos los elementos
-        let element = elements[i];                                      //Se obtiene el elemento actual
+        var element = elements[i];                                      //Se obtiene el elemento actual
 
         if (element.getBoundingClientRect().top < screenSize) {         //Si el elemento esta en la pantalla
             element.classList.add('visible');                           //Se agrega la clase visible
