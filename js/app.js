9a1ee7c3407f4350a95b454df815f535fbcfce3e
Ignacio Picca <piccaignacio@gmail.com>
2022-09-26 22:19:17 -0300

    web terminada!

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@
 // Aparicion de elementos
 
 window.addEventListener('scroll', function () {                         //Al hacer scroll, se ejecuta la funcion
-    var elements = document.getElementsByClassName('scroll-content');   //Se obtienen todos los elementos con la clase scroll-content
-    var screenSize = window.innerHeight;                                //Se obtiene el tamaño de la pantalla               
+    let elements = document.getElementsByClassName('scroll-content');   //Se obtienen todos los elementos con la clase scroll-content
+    let screenSize = window.innerHeight;                                //Se obtiene el tamaño de la pantalla               
 
-    for (var i = 0; i < elements.length; i++) {                         //Se recorren todos los elementos
-        var element = elements[i];                                      //Se obtiene el elemento actual
+    for (let i = 0; i < elements.length; i++) {                         //Se recorren todos los elementos
+        let element = elements[i];                                      //Se obtiene el elemento actual
 
         if (element.getBoundingClientRect().top < screenSize) {         //Si el elemento esta en la pantalla
             element.classList.add('visible');                           //Se agrega la clase visible
